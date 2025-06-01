2276f16c013d5e12331651796706ed3d6267fa16
Dilary18 <dilarycruz2015@gmail.com>
2025-06-01 13:01:10 -0600

    correcion home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
-import Home from './pages/Home'
+import Home from './pages/Home.jsx'
 import Cart from './pages/Cart'
 import Favorites from './pages/Favorites'
 import ProductDetail from './pages/ProductDetail'
