5c4b8d9ebf559f16b9cd9a9eb70dfb8c5cfd5612
Dilary18 <dilarycruz2015@gmail.com>
2025-06-01 14:31:33 -0600

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Header from './components/Header'
-import Home from './pages/Home.jsx'
-import Cart from './pages/Cart'
-import Favorites from './pages/Favorites'
-import ProductDetail from './pages/ProductDetail'
+import Header from '/src/components/Header.jsx'
+import Home from '/src/pages/Home.jsx'
+import Cart from '/src/pages/Cart.jsx'
+import Favorites from '/src/pages/Favorites.jsx'
+import ProductDetail from '/src/pages/ProductDetail.jsx'
 import { Routes, Route } from 'react-router-dom'
-import { CartProvider } from './context/CartContext'
-import { FavProvider } from './context/FavContext'
+import { CartProvider } from '/src/context/CartContext.jsx'
+import { FavProvider } from '/src/context/FavContext.jsx'
 
 export default function App() {
   return (
